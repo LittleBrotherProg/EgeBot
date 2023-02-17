05c4d84179e601a5e1e72350731ad5e570fe0478
Grisha_Work <littlebrotherprog@gmail.com>
2023-02-17 18:26:56 +0300

    pre alfa

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 import telebot
-
-bot = telebot.TeleBot('', 
+from telebot import types
+import config
+import pars
+with open('token.txt') as token:
+	token = token.read()
+bot = telebot.TeleBot(token, 
 		            parse_mode=None)
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	btn1 = types.KeyboardButton("👋 Поздороваться")
+	btn2 = types.KeyboardButton("Выбор предмета")
+	markup.add(btn1, btn2)
+	bot.send_message(
+					message.chat.id, 
+		  			text='Приветствую, я бот помощник к успешной сдачи ЕГЭ',
+                    reply_markup=markup
+					)
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == "👋 Поздороваться"):
+        bot.send_message(message.chat.id, text="Привеет.. Спасибо что пользуешся мной!)")
+    elif(message.text == "Выбор предмета"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Информатика")
+        btn2 = types.KeyboardButton("Математика")
+        back = types.KeyboardButton("Физика")
+        markup.add(btn1, btn2, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+    elif(message.text == "Информатика"):
+        print('lol')
+        u = pars.pars_link('https://inf-ege.sdamgia.ru/')
+        answer = u.pars_link_task()
+        for i, button in enumerate(answer):
+            print(button)
+            print('-' * 20)
+            print(button)
+             
 	
 bot.infinity_polling()
--- pars.py
+++ pars.py
@@ -3,39 +3,59 @@ from selenium_driver_updater import DriverUpdater
 from selenium.webdriver.common.by import By
 import time
 import json
-filename = DriverUpdater.install(driver_name=DriverUpdater.chromedriver)
-driver = webdriver.Chrome(filename)
-url = 'https://inf-ege.sdamgia.ru/'
-pars_link = [{'link_task': {}}]
-driver.get('https://inf-ege.sdamgia.ru/')
-time.sleep(5)
-parent  =  driver.find_element(By.CLASS_NAME,  'Constructor-PartList')
-childs_one = parent.find_elements(By.CLASS_NAME, 'ConstructorForm-Row')
-del childs_one[0]
-for i, element in enumerate(childs_one):
-    element = element.find_elements(By.CLASS_NAME, 'ConstructorForm-Topic')[0]
-    time.sleep(0.5)
-    element.click()
-    try:
-        father_name_link = (element.find_elements(By.TAG_NAME, 'u')
-                )[0].get_property('outerText')
-    except IndexError:
-        father_name_link = (element.find_elements(
-                                                  By.CLASS_NAME, 
-                                                  'ConstructorForm-TopicDesc'
-                                                  ))[0].get_attribute('innerText')
-    pars_link[0]['link_task'].update({father_name_link:{}})
-    info_child_name_link = element.find_elements(
-                                            By.CLASS_NAME, 
-                                            'Link_wrap.ConstructorForm-TopicName.Label'
-                                            )
-    for child_name_link in info_child_name_link:
-        name = ((child_name_link.get_attribute('outerText')).split('\xa0'))[0]
-        link = child_name_link.find_elements(By.TAG_NAME, 'a')
-        if len(link) != 0 and name != '':
-            link = link[0].get_attribute('href')
-            pars_link[0]['link_task'][father_name_link].update({name: link})
-    if i == 27:
-        print(pars_link)
-        break
+
+class pars_link():
+
+    def __init__(self, url) :
+        self.filename =DriverUpdater.install(driver_name=DriverUpdater.chromedriver)
+        self.url = url
     
+    def pars_link_task(self):
+        driver = webdriver.Chrome(self.filename)
+        link_task = 'link task'
+        link_task2 = 'links to tasks not included in this year'
+        pars_link = [
+                    {link_task: {}}, 
+                    {link_task2:{}}
+                    ]
+        driver.get('https://inf-ege.sdamgia.ru/')
+        time.sleep(5)
+        parent  =  driver.find_element(By.CLASS_NAME,  'Constructor-PartList')
+        childs_one = parent.find_elements(By.CLASS_NAME, 'ConstructorForm-Row')
+        del childs_one[0]
+        for i, element in enumerate(childs_one):
+            element = element.find_elements(By.CLASS_NAME, 'ConstructorForm-Topic')[0]
+            time.sleep(0.5)
+            element.click()
+            try:
+                father_name_link = (element.find_elements(By.TAG_NAME, 'u')
+                        )[0].get_property('outerText')
+            except IndexError:
+                father_name_link = (element.find_elements(
+                                                        By.CLASS_NAME, 
+                                                        'ConstructorForm-TopicDesc'
+                                                        ))[0].get_attribute('innerText')
+            if i <= 26:
+                update = link_task
+                numb = 0
+            else:
+                update = link_task2
+                numb = 1
+            pars_link[numb][update].update({father_name_link:{}})
+            info_child_name_link = element.find_elements(
+                                                        By.CLASS_NAME, 
+                                                        'Link_wrap.ConstructorForm-TopicName.Label'
+                                                        )
+            for child_name_link in info_child_name_link:
+                name = ((child_name_link.get_attribute('outerText')).split('\xa0'))[0]
+                link = child_name_link.find_elements(By.TAG_NAME, 'a')
+                if len(link) != 0 and name != '':
+                    link = link[0].get_attribute('href')
+                    pars_link[numb][update][father_name_link].update({name: link})
+        return pars_link
+
+# u = pars_link('https://inf-ege.sdamgia.ru/')
+# answer = u.pars_link_task()
+# print(answer[0])
+
+            
