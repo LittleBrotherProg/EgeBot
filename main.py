f131221eecbca14399c2924f9765bf27341b6ab2
G.K_home <littlebrotherprog@gmail.com>
2023-02-20 04:48:07 +0300

    redoing the parser

--- main.py
+++ main.py
@@ -6,39 +6,60 @@ with open('token.txt') as token:
 	token = token.read()
 bot = telebot.TeleBot(token, 
 		            parse_mode=None)
-@bot.message_handler(commands=['start'])
+
+params = ['Тестовая часть', 'Развёрнутая часть', 'Задания, не входящие в ЕГЭ этого года']
+@bot.message_handler(commands=['start'], content_types=['text'])
 def send_welcome(message):
-	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	markup_welcom = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	btn1 = types.KeyboardButton("👋 Поздороваться")
 	btn2 = types.KeyboardButton("Выбор предмета")
-	markup.add(btn1, btn2)
+	markup_welcom.add(btn1, btn2)
 	bot.send_message(
 					message.chat.id, 
 		  			text='Приветствую, я бот помощник к успешной сдачи ЕГЭ',
-                    reply_markup=markup
+                    reply_markup=markup_welcom
 					)
 @bot.message_handler(content_types=['text'])
 def func(message):
     if(message.text == "👋 Поздороваться"):
         bot.send_message(message.chat.id, text="Привеет.. Спасибо что пользуешся мной!)")
     elif(message.text == "Выбор предмета"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Информатика")
-        btn2 = types.KeyboardButton("Математика")
-        back = types.KeyboardButton("Физика")
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
-    elif(message.text == "Информатика"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Тестовая часть")
-        btn2 = types.KeyboardButton("Задания, не входящие в ЕГЭ этого года")
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text="Выберите задание", reply_markup=markup)
-        u = pars.pars_link('https://inf-ege.sdamgia.ru/')
-        answer = u.pars_link_task()
-        for i, button in enumerate(answer):
-            print(button['link task'])
-            print('-' * 20)
+        markup = types.InlineKeyboardMarkup(row_width=3)
+        btn1 = types.InlineKeyboardButton("Информатика", callback_data=[params[0,2], 'Информатика'])
+        btn2 = types.InlineKeyboardButton("Математика", callback_data='but2')
+        btn3 = types.InlineKeyboardButton("Физика", callback_data='but3')
+        btn4 = types.InlineKeyboardButton("Химия", callback_data='but4')
+        btn5 = types.InlineKeyboardButton("Биология", callback_data='but5')
+        btn6 = types.InlineKeyboardButton("География", callback_data='but6')
+        btn7 = types.InlineKeyboardButton("Общество знание", callback_data='but7')
+        btn8 = types.InlineKeyboardButton("Литература", callback_data='but8')
+        btn9 = types.InlineKeyboardButton("История", callback_data='but9')
+        markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
+        bot.send_message(message.chat.id, text="Выбери предмет:", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+     if call.data[0] == params[0,1] and call.data[1] == :
+            new_menu = types.ReplyKeyboardMarkup()
+            test02 = types.KeyboardButton('Тестовая часть',)
+            new_menu.add(types.InlineKeyboardButton('Ничего нового', callback_data='but4'))
+            bot.edit_message_text('Новое меню, кнопка 1', call.message.chat.id, call.message.message_id,
+                              reply_markup=new_menu)
+         
+    # elif(message.text == "Информатика"):
+    #     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    #     btn1 = types.KeyboardButton("Тестовая часть")
+    #     btn2 = types.KeyboardButton("Задания, не входящие в ЕГЭ этого года")
+    #     btn3 = types.KeyboardButton("Развёрнутая часть")
+    #     markup.add(btn1, btn2, back)
+    #     bot.send_message(message.chat.id, text="Выберите задание", reply_markup=markup)
+    #     u = pars.pars_link('https://inf-ege.sdamgia.ru/')
+    #     answer = u.pars_link_task()
+    #     for i, button in enumerate(answer):
+    #         print(button['link task'])
+    #         print('-' * 20)
+    # elif (message.text == "Назад"):
+    #     bot.send_message(message.chat.id, "Вы вернулись в меню",reply_markup=None)
             
              
 	
--- pars.py
+++ pars.py
@@ -5,25 +5,32 @@ import time
 import json
 
 class pars_link():
-
     def __init__(self, url) :
         self.filename =DriverUpdater.install(driver_name=DriverUpdater.chromedriver)
         self.url = url
     
-    def pars_link_task(self):
-        driver = webdriver.Chrome(self.filename)
-        link_task = 'link task'
-        link_task2 = 'links to tasks not included in this year'
-        pars_link = [
-                    {link_task: {}}, 
-                    {link_task2:{}}
-                    ]
-        driver.get('https://inf-ege.sdamgia.ru/')
-        time.sleep(5)
-        parent  =  driver.find_element(By.CLASS_NAME,  'Constructor-PartList')
-        childs_one = parent.find_elements(By.CLASS_NAME, 'ConstructorForm-Row')
-        del childs_one[0]
-        for i, element in enumerate(childs_one):
+    def pars_element(self):
+            driver = webdriver.Chrome(self.filename)
+            driver.get(url)
+            time.sleep(5)
+            parent  =  driver.find_element(By.CLASS_NAME,  'Constructor-PartList')
+            childs_one = parent.find_elements(By.CLASS_NAME, 'ConstructorForm-Row')
+            return childs_one
+
+    def father_link(self, childs_one):
+        for element, item in zip(childs_one, items[0]):
+            link_task = 'Тестовая часть'
+            link_task2 = 'Развёрнутая часть'
+            link_task3 = 'Задания, не входящие в ЕГЭ этого года'
+            pars_link = [
+                        {
+                        item:[
+                            {link_task: {}}, 
+                            {link_task2:{}},
+                            {link_task3:{}}
+                            ]
+                        }
+                        ]
             element = element.find_elements(By.CLASS_NAME, 'ConstructorForm-Topic')[0]
             time.sleep(0.5)
             element.click()
@@ -35,27 +42,47 @@ class pars_link():
                                                         By.CLASS_NAME, 
                                                         'ConstructorForm-TopicDesc'
                                                         ))[0].get_attribute('innerText')
-            if i <= 26:
+            if link_task == father_name_link:
                 update = link_task
                 numb = 0
-            else:
+                continue
+            elif link_task2 == father_name_link:
                 update = link_task2
                 numb = 1
-            pars_link[numb][update].update({father_name_link:{}})
+                continue
+            elif link_task3 == father_name_link:
+                update = link_task2
+                numb = 2
+                continue
+            pars_link[0][item][numb][update].update({father_name_link:{}})
             info_child_name_link = element.find_elements(
                                                         By.CLASS_NAME, 
                                                         'Link_wrap.ConstructorForm-TopicName.Label'
                                                         )
-            for child_name_link in info_child_name_link:
+            return [info_child_name_link, pars_link, father_name_link, update, numb]
+    
+    def pars_links(self):
+        childs_one = pl.pars_element()
+        father_link = pl.father_link(childs_one)
+        pars_link = father_link[1]
+        update = father_link[3]
+        numb = father_link[4]
+        father_name_link = father_link[2]
+        info_child_name_link = father_link[0]
+        for child_name_link in info_child_name_link:
                 name = ((child_name_link.get_attribute('outerText')).split('\xa0'))[0]
                 link = child_name_link.find_elements(By.TAG_NAME, 'a')
                 if len(link) != 0 and name != '':
                     link = link[0].get_attribute('href')
                     pars_link[numb][update][father_name_link].update({name: link})
         return pars_link
-
-# u = pars_link('https://inf-ege.sdamgia.ru/')
-# answer = u.pars_link_task()
-# print(answer[0])
+with open('father_link.json', encoding="utf8") as fl:
+                items = json.load(fl)
+                keys = items[0].keys()
+for key in keys:
+      url = items[0].get(key)
+      pl = pars_link(url)
+      answer = pl.pars_links()
+print(answer[0])
 
             
